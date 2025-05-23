e99486bd4118e66027eb62d89d65d2459ee42bce
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 14:20:07 +0000

    Definição da estrutura da Carta do jogo

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,15 @@
 
 int main(){
     printf("Desafio Xadrez!\n");
+}
 
+struct Carta {
+    char codigo[4];
+    char estado;
+    char nome_cidade;
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
 
 }
